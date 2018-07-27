2e3d6c1a6e8522572bcb01bbce693ee9cb03e98f
wenboya <1330166936@qq.com>
2018-07-27 10:23:05 +0800

    yy

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,9 +1,7 @@
 public class Demo {
-    public static void main(String[] args){
-        System.out.println("hello world!");
-
-
-    }
+   public static void main(String[] args){
+        System.out.println();
+   }
 
 
 }
